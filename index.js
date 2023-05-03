ed98fdc4eae4719d7d19d3fdac13a075623cad7c
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2023-05-03 09:38:08 +0200

    read server port from env

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ const load_model = async () => {
 }
 
 // Keep the model in memory, make sure it's loaded only once
-load_model().then(() => app.listen(8080))
+load_model().then(() => app.listen(process.env.PORT || 8080))
 
 // curl --request POST localhost:8080/nsfw --header 'Content-Type: multipart/form-data' --data-binary 'image=@/full/path/to/picture.jpg'
